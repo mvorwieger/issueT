217b5178f75040e62f27b28910f5996fb4ee1056
mvorwieger <michel.vorwieger@outlook.de>
2018-11-30 22:28:02 +0100

    added some comments that describe the functions

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const todoRegExp = () => new RegExp(/^(.*)TODO: (.*)$/gm);
 const gitHubUrlRegExp = () => new RegExp(/^(.*)url = (.*)$/gm);
 const githubApi = 'api.github.com';
 
+/**
+ * Gives you the TODO comments from the given file 
+ * @param {string} pathToFile 
+ */
 const getTodoCommentFromFile = pathToFile => {
   return new Promise((res, rej) => {
     fs.readFile(pathToFile, (err, data) => {
@@ -18,7 +22,7 @@ const getTodoCommentFromFile = pathToFile => {
       if (allTodoComments) {
         const regexGroups = allTodoComments.map(
           comment => { return todoRegExp().exec(comment); });
-        res(regexGroups);
+        res(regexGroups.map((regexobj) => getStuffOutOfRegExObj(regexobj)))
       } else {
         rej("no Todo Comments Found")
       }
@@ -26,12 +30,22 @@ const getTodoCommentFromFile = pathToFile => {
   });
 };
 
+/**
+ * A helper function to parse out the comments from the RegEx Arrays 
+ * @param {Array<any>} regexthingy 
+ */
 const getStuffOutOfRegExObj = regexthingy => {
   const [fullComment, _, comment, ...rest] = regexthingy;
 
   return { fullComment, comment };
 };
 
+/**
+ * Gives the current origin url from .git/config 
+ * this might be a problem later on because it only works 
+ * in the root path of the project (the location where .git/config is located)
+ * @param {string} path 
+ */
 const getGithubUrlInPath = path => {
   return new Promise(((resolve, reject) => {
     fs.readFile(path + '/.git/config', (err, fileBuffer) => {
@@ -49,44 +63,56 @@ const getGithubUrlInPath = path => {
   }));
 };
 
+/**
+ * Gives you the current Open Issues for your origin 
+ * @param {string} username 
+ * @param {string} password 
+ * @param {string} githubApi 
+ * @param {string} repoPath 
+ * @returns {Promise<string[]>}
+ */
 const getIssues = (username, password, githubApi, repoPath) => {
   return axios
     .get(`https://${username}:${password}@${githubApi}/repos/${
       repoPath}/issues`)
 }
 
-const postIssue = (username, password, githubApi,
-  repoPath, title, body) => {
+/**
+ * Posts a Issue to Github with the given title and body
+ * @param {string} username 
+ * @param {string} password 
+ * @param {string} githubApi 
+ * @param {string} repoPath 
+ * @param {string} title 
+ * @param {string} body 
+ */
+const postIssue = (username, password, githubApi, repoPath, title, body) => {
   const issueDetails = {
     title,
     body,
   }
 
-  const url =
-    `https://${username}:${password}@${githubApi}/repos${repoPath}/issues`
+  const url = `https://${username}:${password}@${githubApi}/repos${repoPath}/issues`
 
   return axios.post(url, (issueDetails))
 }
 
 (async () => {
+  //TODO: Look if there is some cleaner way to get arguments
+  //TODO: Look for a way to cache username and password
   const [nodeLocation, currentPath, filePath, username, password] = process.argv;
-  console.log(filePath, username, password);
-  const regExObjects = await getTodoCommentFromFile(filePath);
-  const comments =
-    regExObjects.map((regexobj) => getStuffOutOfRegExObj(regexobj));
+  const comments = await getTodoCommentFromFile(filePath);
   const gitHubUrl = await getGithubUrlInPath('./');
-  const repoPath = gitHubUrl.split('github.com')[1];
-  const splitAwayDotGit = repoPath.split(".git")[0]
+  const repoPath = gitHubUrl.split('github.com')[1].split(".git")[0];
 
+  // TODO: First get all Comments and dont post duplicates
   // TODO: Add some option to add body to a github issue instead of it just being the same as the body
-
   comments.forEach(async (comment, _, arr) => {
     try {
-      const posted = await postIssue(username, password, githubApi, splitAwayDotGit, comment.comment, comment.comment)
+      const posted = await postIssue(username, password, githubApi, repoPath, comment.comment, comment.comment)
     } catch (e) {
       console.log(e)
     }
-
   })
 
   console.log(`${comments.length} issues created`);
