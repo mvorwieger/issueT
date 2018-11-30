15eea4b535ae00b13eeb5a7b0795bd3f15747734
mvorwieger <michel.vorwieger@outlook.de>
2018-11-30 21:27:09 +0100

    added path as first arg

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
+#!/usr/bin/env node
 // TODO: get filepath from command line args
-import axios from "axios"
-import fs from "fs"
+const axios = require("axios");
+const fs = require("fs");
 
-const filePath = './examples/index.js';
 const todoRegExp = () => new RegExp(/^(.*)TODO: (.*)$/gm);
 const gitHubUrlRegExp = () => new RegExp(/^(.*)url = (.*)$/gm);
 const githubApi = 'api.github.com';
 
 const getTodoCommentFromFile =
-    (pathToFile) => {
-      return new Promise((res, rej) => {
-        fs.readFile(pathToFile, (err, data) => {
-          if (err) {
-            rej(err);
-          }
+  (pathToFile) => {
+    return new Promise((res, rej) => {
+      fs.readFile(pathToFile, (err, data) => {
+        if (err) {
+          rej(err);
+        }
 
-          const fileContent = (data.toString());
-          const allTodoComments = fileContent.match(todoRegExp());
-          if (allTodoComments) {
-            const regexGroups = allTodoComments.map(
-                comment => { return todoRegExp().exec(comment); });
-            res(regexGroups);
-          } else {
-            rej("no Todo Comments Found")
-          }
-        });
+        const fileContent = (data.toString());
+        const allTodoComments = fileContent.match(todoRegExp());
+        if (allTodoComments) {
+          const regexGroups = allTodoComments.map(
+            comment => { return todoRegExp().exec(comment); });
+          res(regexGroups);
+        } else {
+          rej("no Todo Comments Found")
+        }
       });
-    };
+    });
+  };
 
 const getStuffOutOfRegExObj = (regexthingy) => {
   const [fullComment, _, comment, ...rest] = regexthingy;
 
-  return {fullComment, comment};
+  return { fullComment, comment };
 };
 
-const getGithubUrlInPath = (path: string) => {
+const getGithubUrlInPath = (path) => {
   return new Promise(((resolve, reject) => {
     fs.readFile(path + '/.git/config', (err, fileBuffer) => {
       if (err) {
@@ -52,31 +52,31 @@ const getGithubUrlInPath = (path: string) => {
 };
 
 const getIssues = (username, password, githubApi, repoPath) => {
-          axios
-              .get(`https://${username}:${password}@${githubApi}/repos/${
-                  repoPath}/issues`)
-              .then(res => {console.log(res)})
-              .catch(e => { console.log(e); });
-        }
+  axios
+    .get(`https://${username}:${password}@${githubApi}/repos/${
+      repoPath}/issues`)
+    .then(res => { console.log(res) })
+    .catch(e => { console.log(e); });
+}
 
 const postIssue = (username, password, githubApi,
-                   repoPath, title, body) => {
-      const issueDetails = {
-        title,
-        body,
-      }
+  repoPath, title, body) => {
+  const issueDetails = {
+    title,
+    body,
+  }
 
-      const url =
-          `https://${username}:${password}@${githubApi}/repos${repoPath}/issues`
+  const url =
+    `https://${username}:${password}@${githubApi}/repos${repoPath}/issues`
 
-      return axios.post(url, (issueDetails))
-    }
+  return axios.post(url, (issueDetails))
+}
 
 (async () => {
-  const [nodeLocation, currentPath, username, password] = process.argv;
+  const [nodeLocation, currentPath, filePath, username, password] = process.argv;
   const regExObjects = await getTodoCommentFromFile(filePath);
   const comments =
-      regExObjects.map((regexobj) => getStuffOutOfRegExObj(regexobj));
+    regExObjects.map((regexobj) => getStuffOutOfRegExObj(regexobj));
   const gitHubUrl = await getGithubUrlInPath('./');
   const repoPath = gitHubUrl.split('github.com')[1];
   const splitAwayDotGit = repoPath.split(".git")[0]
@@ -87,8 +87,8 @@ const postIssue = (username, password, githubApi,
   comments.forEach(async (comment) => {
     try {
       const posted =
-          await postIssue(username, password, githubApi, splitAwayDotGit,
-                          comment.comment, comment.comment)
+        await postIssue(username, password, githubApi, splitAwayDotGit,
+          comment.comment, comment.comment)
       console.log(posted)
     } catch (e) {
       console.log(e)
